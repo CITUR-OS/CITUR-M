9b1834587f6c6d2862b253f8b0e19ec46c723728
Elisha Hollander <just4now666666@gmail.com>
2021-02-18 02:05:41 +0200

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 OS=$PWD
-git clone https://github.com/donno2048/buildroot.git
+git clone https://github.com/CITUR-OS/buildroot.git
 mv buildroot buildroot_x86-64
 cd buildroot_x86-64
 make BR2_EXTERNAL="$OS/external" "x86-64_defconfig"
